e1b6f37125733e876714fe88f6d1ab15211845d9
Alexandre <alexandre.anastassiades@tutanota.com>
2020-04-28 09:12:34 +0200

    Ajouts de tests pour le visuel de l'IHM

---
+++ tests/tests/tests/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tests
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Sah quel plaisir";
+        }*/
+        void OnEntry(object sender, TextChangedEventArgs e)
+        {
+            string old = e.OldTextValue;
+            string newt = e.NewTextValue; 
+        }
+        void Completed(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
