49880b46b9ff5f34794c6250c7e8dce57494febe
Alexandre <alexandre.anastassiades@tutanota.com>
2020-04-29 08:20:55 +0200

    Ajout du fichier IHM
    Projet uniquement android (sans modifications particulière par rapport au fichier test)

---
+++ IHM/IHM/IHM/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace IHM
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isConnected = false;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonSendClicked(object sender, EventArgs e)
+        {
+            //To Indent 
+
+        }
+        void OnButtonReceiveClicked(object sender, EventArgs e)
+        {
+            //To Indent
+        }
+        void OnButtonConnectClicked(object sender, EventArgs e)
+        {
+            //To Indent (method to connect)
+
+            if (isConnected == true)
+            {
+                connectButton.IsEnabled = false;
+                receiveButton.IsEnabled = true;
+                sendButton.IsEnabled = true;
+                disconnectButton.IsEnabled = true;
+            }
+        }
+        void OnButtonDisconnectClicked(object sender, EventArgs e)
+        {
+            //To Indent (method to disconnect)
+
+            if (isConnected == false)
+            {
+                connectButton.IsEnabled = true;
+                receiveButton.IsEnabled = false;
+                sendButton.IsEnabled = false;
+                disconnectButton.IsEnabled = false;
+            }
+        }
+    }
+}
