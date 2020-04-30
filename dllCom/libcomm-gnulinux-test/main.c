3ca915d029a2d7a732037c63291865f806f7cf64
Julien Vernay <julien.vernay@protonmail.com>
2020-04-30 12:09:24 +0200

    Réécriture test GNU/Linux

--- dllCom/libcomm-gnulinux-test/main.c
+++ dllCom/libcomm-gnulinux-test/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <protocomm-gnulinux.h>
+#include <protocomm-details.h>
 
 #ifdef __STDC_NO_THREADS__
 #error "This code needs C11 <threads.h> to compile"
@@ -12,17 +13,17 @@
 #include <assert.h>
 
 //========= CODE ESCLAVE ============
-typedef struct DeviceInfo {
-	proto_Device device;
+typedef struct DeviceInfo_t {
+	proto_Device_t device;
 	void* iodata;
-} DeviceInfo;
+} DeviceInfo_t;
 
 // Le slave reçoit une demande GET [R] et retourne R*3.
 // Les demandes SET échouent toujours.
-// userdata doit être un pointeur vers HowToReply
-void slave_receive(void* userdata, proto_Command command, uint8_t const* args_in) {
+// userdata doit être un pointeur vers DeviceInfo_t
+void slave_receive(void* userdata, proto_Command_t command, uint8_t const* args_in) {
 	uint8_t args_out[proto_MAX_ARGS];
-	DeviceInfo const* devInfo = userdata;
+	DeviceInfo_t const* devInfo = userdata;
 	switch(command) {
 	case proto_GET:
 		args_out[0] = args_in[0] * 3;
@@ -45,8 +46,8 @@ mtx_t slaveStopMtx;
 int slaveStop = 0;
 
 int slaveThreadFunc(void* data) {
-	DeviceInfo* devInfo = data;
-	proto_State state = { 0 };
+	DeviceInfo_t* devInfo = data;
+	proto_State_t state = { 0 };
 	proto_setReceiver(&state, slave_receive, devInfo);
 	
 	while(1) { // tant que slaveStop != 0
@@ -63,29 +64,11 @@ int slaveThreadFunc(void* data) {
 }
 
 
-//============ CODE MASTER ============
-// Le master reçoit une réponse et l'écrit dans *userdata.
-// userdata doit être un pointeur vers un uint8_t.
-proto_Status master_lastStatus = proto_NO_ERROR;
-void master_receive(void* userdata, proto_Command command, uint8_t const* args) {
-	switch (command) {
-	case proto_REPLY:
-		*(uint8_t*)userdata = args[0];
-		break;
-	case proto_NOTIF_BAD_CRC:
-		printf("Mauvais CRC... reçu=%d, calculé=%d\n", args[0], args[1]);
-		break;
-	case proto_STATUS:
-		master_lastStatus = args[0];
-		break;
-	}
-}
-
-
 
 int main() {
 	// Initialisation des deux devices (un pour le maître et un pour l'esclave)
-	proto_Data_GnuLinux master, slave;
+	proto_Data_GnuLinux_t master, slave;
+	proto_Device_t device = proto_getDevice_GnuLinux(); // même device pour l'esclave et le maître : GNU/Linux
 	
 	int errMaster = proto_initData_GnuLinux(
 			&master, "/dev/ptmx", 0); // le maître n'attend pas
@@ -95,26 +78,25 @@ int main() {
 	}
 	if (grantpt(master.fileDescriptor) < 0 || unlockpt(master.fileDescriptor) < 0) {
 		perror("Erreur en donnant des privilèges au pseudo-terminal");
-		proto_finalizeData_GnuLinux(&master);
+		proto_closeDevice(device, &master);
 		return EXIT_FAILURE;
 	}
-	proto_Device deviceMaster = proto_getDevice_GnuLinux();
 	
 	int errSlave = proto_initData_GnuLinux(
 			&slave, ptsname(master.fileDescriptor), 3); // l'esclave attend au moins 0.3 seconde
 	if (errSlave) {
 		printf("Erreur en ouvrant l'esclave : %d\n", errSlave);
-		proto_finalizeData_GnuLinux(&master);
+		proto_closeDevice(device, &master);
 		return EXIT_FAILURE;
 	}
-	DeviceInfo slaveInfo = { .device = proto_getDevice_GnuLinux(),
-		                     .iodata = &slave };               
+	DeviceInfo_t slaveInfo = { .device = device,
+		                       .iodata = &slave };               
 	
 	// Initialisation du contexte multi-threadé
 	if (mtx_init(&slaveStopMtx, mtx_plain) == thrd_error) {
 		perror("Erreur en créant un mutex");
-		proto_finalizeData_GnuLinux(&master);
-		proto_finalizeData_GnuLinux(&slave);
+		proto_closeDevice(device, &slave);
+		proto_closeDevice(device, &master);
 		return EXIT_FAILURE;
 	}
 	// Lancement du thread de l'esclave
@@ -122,34 +104,27 @@ int main() {
 	if (thrd_create(&slaveThreadId, slaveThreadFunc, &slaveInfo)) {
 		perror("Erreur en créant le thread");
 		mtx_destroy(&slaveStopMtx);
-		proto_finalizeData_GnuLinux(&master);
-		proto_finalizeData_GnuLinux(&slave);
+		proto_closeDevice(device, &slave);
+		proto_closeDevice(device, &master);
 		return EXIT_FAILURE;
 	}
 	
+	//============ CODE MASTER ============
+	
 	// Initialisation du maître
-	proto_State state = { 0 };
 	uint8_t output = 0;
-	proto_setReceiver(&state, master_receive, &output);
 	
-	uint8_t args[proto_MAX_ARGS];
-	
-	// Tout a été correctement initialisé
-	args[0] = 0; args[1] = 0; // on veut écrire 0 dans le registre 0
-	proto_writeFrame(proto_SET, args, deviceMaster, &master);
-	while (!proto_readBlob(&state, deviceMaster, &master));
+	// on veut écrire 0 dans le registre 0
+	proto_Status_t status = proto_cio_master_set(0, 0, /*timeout_ms=*/ 100, device, &master);
 	// On a normalement reçu une erreur : l'esclave utilisé dans ce
 	// programme n'accepte pas les commandes SET !
-	assert(master_lastStatus == proto_INVALID_REGISTER);
+	assert(status == proto_INVALID_REGISTER); 
 	
-	master_lastStatus = proto_NO_ERROR; // réinitialisation
-	
-	args[0] = 28; // on veut lire le "registre" 28
-	proto_writeFrame(proto_GET, args, deviceMaster, &master);
-	while (!proto_readBlob(&state, deviceMaster, &master));
+	// on veut "lire le registre" 28
+	status = proto_cio_master_get(28, &output, 100, device, &master);
+	assert(status == proto_NO_ERROR);
 	// On a normalement reçu 28*3 (cf. implémentation de l'esclave)
 	assert(output == 28*3);
-	assert(master_lastStatus == proto_NO_ERROR); // il n'y a pas eu d'erreur
 	
 	puts("Tout s'est bien passé !");
 	
@@ -160,8 +135,8 @@ int main() {
 	thrd_join(slaveThreadId, NULL);
 	
 	mtx_destroy(&slaveStopMtx);
-	proto_finalizeData_GnuLinux(&master);
-	proto_finalizeData_GnuLinux(&slave);
+	proto_closeDevice(device, &slave);
+	proto_closeDevice(device, &master);
 	return EXIT_SUCCESS;
 }
 
