acc74e228879c912cb444ea27b167d644da60c5d
LIBERADO <selso.liberado@ciose.fr>
2020-05-13 01:41:02 +0200

    iodevice : reprise api mineur, ajout macro d'init

--- dllCom/libcomm/proto_iodevice.h
+++ dllCom/libcomm/proto_iodevice.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef struct proto_IfaceIODevice * proto_Device_t;
 
 /// Instance de base de notre interface Device
 /// Chaque implémentation d'un IO Device devrait fournir une instance de cette interface.
@@ -19,16 +20,16 @@ typedef struct proto_IfaceIODevice {
     ///
     /// @param[in] path d'accès au device (path système)
     /// @return retour de fonction 0=OK, sinon erreur
-    int  (*open)(struct proto_IfaceIODevice* this, const char * szPath);
+    int  (*open)(proto_Device_t this, const char * szPath);
 
     /// @brief clos la liaison
     /// Un open est toujours possible
     ///
-    int (*close)(struct proto_IfaceIODevice* this);
+    int (*close)(proto_Device_t this);
 
     /// Détruit les données de l'interface (par exemple, sous GNU/Linux
     /// cela fera un close() sur le fileDescriptor).
-    void (*destroy)(struct proto_IfaceIODevice* this);
+    void (*destroy)(proto_Device_t this);
 
     /// Lit N caractères. La fonction peut être bloquante
     /// temporairement (timeout) pour certains Device (exemple : GNU/Linux).
@@ -36,21 +37,32 @@ typedef struct proto_IfaceIODevice {
     /// @param[out] buf les octets lus seront écrits ici
     /// @param[in] len taille à lire
     /// @returns >=0 ci qui a put être lu dans le délai imparti, <0 erreur
-    int (*read)(struct proto_IfaceIODevice* this, void* buf, uint8_t len, int16_t tout_ms);
+    int (*read)(proto_Device_t this, void* buf, uint8_t len, int16_t tout_ms);
 
     /// Ecrit N caractères.
     /// @param[in] this pointeur vers l'instance IO Device
     /// @param[in] buffer octets à envoyer
     /// @param[in] size nombre d'octets à envoyer
     /// @returns 0 OK, sinon erreur
-    int (*write)(struct proto_IfaceIODevice* this, const void * buffer, uint8_t size);
+    int (*write)(proto_Device_t this, const void * buffer, uint8_t size);
 
     /// Définit les données complémentaires de l'interface
     void * user;
 } proto_IfaceIODevice_t;
 
 
-typedef proto_IfaceIODevice_t * proto_Device_t;
+/// Macro pour accelérer & uniformiser l'initialisation d'un custom device
+#define DEVIO_INIT(devname, this)\
+{\
+    this->open = devname##_open;\
+    this->close = devname##_close;\
+    this->destroy = devname##_destroy;\
+    this->read = devname##_read;\
+    this->write = devname##_write;\
+}
 
+#ifndef UNUSED
+#define UNUSED(x) (void)x
+#endif
 
 #endif // IODEVICE_H
